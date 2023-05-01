289f011db701c3caadaaa73c2a8af02dc4f3af1a
LeoSrlng <leo.sarlange.pro@outlook.fr>
2023-05-01 21:40:42 +0200

    affichage is typing mais sa bug encore

--- public/JS/ScriptSalon.js
+++ public/JS/ScriptSalon.js
@@ -11,12 +11,9 @@ const formLogin = document.getElementById("formLogin");
 const inputLogin = document.getElementById("inputLogin");
 const loginPage = document.getElementById("Login");
 
-const scrollToBottom = () => {
-	let lastLi = messages.lastChild;
-	if (window.scrollY + window.innerHeight + lastLi.offsetHeight >= document.body.offsetHeight) {
-		window.scrollTo(0, document.body.scrollHeight);
-	}
-};
+let typingTimer;
+let isTyping = false;
+let spanTyping;
 
 formLogin.addEventListener("submit", (e) => {
 	e.preventDefault();
@@ -31,6 +28,41 @@ formLogin.addEventListener("submit", (e) => {
 				inputChat.focus();
 			}
 		});
+	}
+});
+
+socket.on("err-log", (err) => {
+	let reponse = document.createElement("p");
+	reponse.className = "err-msg";
+	reponse.textContent = err;
+
+	loginPage.appendChild(reponse);
+
+	scrollToBottom();
+});
+
+socket.on("user-login", (user) => {
+	let nbrUser = users.childNodes.length;
+	let aUser = document.createElement("li");
+	aUser.className = user.username + " new";
+	aUser.textContent = user.username;
+
+	spanTyping = document.createElement("span");
+	spanTyping.className = "typing";
+	spanTyping.textContent = " ...est en train d'écrire";
+
+	users.appendChild(aUser);
+	aUser.appendChild(spanTyping);
+	spanTyping.hidden = true;
+
+	setTimeout(() => {
+		if (aUser.classList.contains("new")) {
+			aUser.classList.remove("new");
+		}
+	}, 1250);
+	if (users.firstChild.className.includes(user.username) && nbrUser > 0) {
+		let toRemove = document.querySelector("li." + user.username);
+		toRemove.remove();
 	}
 });
 
@@ -65,6 +97,37 @@ socket.on("chat message", (msg) => {
 	scrollToBottom();
 });
 
+inputChat.onkeydown = () => {
+	clearTimeout(typingTimer);
+	if (!isTyping) {
+		socket.emit("start-typing");
+		isTyping = true;
+	}
+};
+
+inputChat.onkeyup = () => {
+	clearTimeout(typingTimer);
+	typingTimer = setTimeout(() => {
+		if (isTyping) {
+			socket.emit("stop-typing");
+			isTyping = false;
+		}
+	}, 750);
+};
+
+socket.on("update-typing", (typingUsers) => {
+	console.log(typingUsers);
+	// let toHide = document.querySelector("span.typing");
+	spanTyping.hidden = true;
+	// console.log(toHide);
+	for (index = 0; index < typingUsers.length; index++) {
+		let toShow = document.querySelector("li." + typingUsers[index].username + " span.typing");
+		toShow.hidden = false;
+		console.log("je passe ici");
+	}
+	// console.log(toShow);
+});
+
 socket.on("service-message", (msg) => {
 	let resContainer = document.createElement("li");
 	resContainer.className = msg.type;
@@ -79,33 +142,12 @@ socket.on("service-message", (msg) => {
 	scrollToBottom();
 });
 
-socket.on("err-log", (err) => {
-	let reponse = document.createElement("p");
-	reponse.className = "err-msg";
-	reponse.textContent = err;
-
-	loginPage.appendChild(reponse);
-
-	scrollToBottom();
-});
-
-socket.on("user-login", (user) => {
-	let nbrUser = users.childNodes.length;
-	let aUser = document.createElement("li");
-	aUser.className = user.username + " new";
-	aUser.textContent = user.username;
-	users.appendChild(aUser);
-
-	setTimeout(() => {
-		if (aUser.classList.contains("new")) {
-			aUser.classList.remove("new");
-		}
-	}, 1250);
-	if (users.firstChild.className.includes(user.username) && nbrUser > 0) {
-		let selector = document.querySelector("li." + user.username);
-		selector.remove();
+const scrollToBottom = () => {
+	let lastLi = messages.lastChild;
+	if (window.scrollY + window.innerHeight + lastLi.offsetHeight >= document.body.offsetHeight) {
+		window.scrollTo(0, document.body.scrollHeight);
 	}
-});
+};
 
 socket.on("user-logout", (user) => {
 	let selector = document.querySelector("li." + user.username);
--- server.js
+++ server.js
@@ -11,13 +11,15 @@ app.use("/", router);
 app.use(express.static("public"));
 
 let users = [];
+let messages = [];
+let typingUsers = [];
 
 io.on("connection", (socket) => {
 	console.log("Un utilisateur débarque");
 	let loggedUser;
 
 	socket.on("user-login", (user, callback) => {
-		let err = "Le nom d'utilisateur \"" + user.username + '" est déja utilisé ';
+		let err = "Le nom d'utilisateur \"" + user.username + '" est déja utilisé';
 		let userIndex = -1;
 		for (i = 0; i < users.length; i++) {
 			if (users[i].username === user.username) {
@@ -28,44 +30,79 @@ io.on("connection", (socket) => {
 			loggedUser = user;
 			users.push(loggedUser);
 			let userServiceMessage = {
-				text: loggedUser.username + " s'est connecté",
+				text: loggedUser.username + " est de retour, pour vous jouer un mauvais tour",
 				type: "login",
 			};
 			let broadcastedServiceMessage = {
-				text: loggedUser.username + " s'est connecté",
+				text: loggedUser.username + " est de retour, pour vous jouer un mauvais tour",
 				type: "login",
 			};
 			socket.emit("service-message", userServiceMessage);
 			socket.broadcast.emit("service-message", broadcastedServiceMessage);
+			messages.push(broadcastedServiceMessage);
 			for (ii = 0; ii < users.length; ii++) {
 				io.emit("user-login", users[ii]);
 			}
 			callback(true);
-			console.log(loggedUser.username + " s'est connecter");
+			console.log(loggedUser.username + " est de retour, pour vous jouer un mauvais tour");
 		} else {
 			socket.emit("err-log", err);
 			callback(false);
 		}
 	});
 
+	for (iii = 0; iii < messages.length; iii++) {
+		if (messages[iii].username !== undefined) {
+			socket.emit("chat message", messages[iii]);
+		} else {
+			socket.emit("service-message", messages[iii]);
+		}
+	}
+
+	socket.on("start-typing", () => {
+		if (typingUsers.indexOf(loggedUser) === -1) {
+			typingUsers.push(loggedUser);
+		}
+		console.log(typingUsers);
+		io.emit("update-typing", typingUsers);
+	});
+
+	socket.on("stop-typing", () => {
+		let typingUserIndex = typingUsers.indexOf(loggedUser);
+		if (typingUserIndex !== -1) {
+			typingUsers.splice(typingUserIndex, 1);
+		}
+		console.log(typingUsers);
+		io.emit("update-typing", typingUsers);
+	});
+
 	socket.on("chat message", (msg) => {
 		msg.username = loggedUser.username;
 		io.emit("chat message", msg);
+		messages.push(msg);
+		if (messages.length > 150) {
+			messages.splice(0, 1);
+		}
 	});
 
 	socket.on("disconnect", () => {
 		if (loggedUser !== undefined) {
 			let serviceMessage = {
-				text: loggedUser.username + " dégage de là parce qu'il est un connard et un putain de prof de français",
+				text: "Une fois de plus " + loggedUser.username + " s'envole vers d'autres cieux",
 				type: "logout",
 			};
+			let typingUserIndex = typingUsers.indexOf(loggedUser);
+			if (typingUserIndex !== -1) {
+				typingUsers.splice(typingUserIndex, 1);
+			}
 			socket.broadcast.emit("service-message", serviceMessage);
 			let userIndex = users.indexOf(loggedUser);
 			if (userIndex !== -1) {
 				users.splice(userIndex, 1);
 			}
+			messages.push(serviceMessage);
 			io.emit("user-logout", loggedUser);
-			console.log(loggedUser.username + " s'est déconnecter");
+			console.log("Une fois de plus " + loggedUser.username + " s'envole vers d'autres cieux");
 		}
 	});
 });
